4a8fe93e3511cf4b8b5ad855c92e8a0dca6951b7
Nicolas-Sy <47263252+Nicolas-Sy@users.noreply.github.com>
2019-10-23 18:17:13 +0800

    all the table columns
    table columns

--- models/document-tracker.js
+++ models/document-tracker.js
@@ -58,34 +58,36 @@ async function gsrun(cl){
     console.log(tableHeader);
 }*/ 
 
-// let docsRows; 
+/*
 
-// const GoogleSpreadsheet = require('google-spreadsheet');
-// const {promisify} = require('util');
+ let docsRows; 
 
-// const creds = require ('../documentTracker.json');
-// const doc = new GoogleSpreadsheet('1z_6sjroYPL8_TzMf6BGqWIoDzQoC8dnAIDgRjVVTNPQ');
+ const GoogleSpreadsheet = require('google-spreadsheet');
+const {promisify} = require('util');
+
+const creds = require ('../documentTracker.json');
+const doc = new GoogleSpreadsheet('1z_6sjroYPL8_TzMf6BGqWIoDzQoC8dnAIDgRjVVTNPQ');
 
 // function printDocument(docs){
-//     console.log(`Timestamp: ${docs.timestamp}`);
-//     console.log(`Date of Submission: ${docs.dateofsubmission}`);
-//     console.log(`Submitted By: ${docs.submittedby}`);
-//     console.log(`Organization's Name: ${docs.organizationsname}`);
-//     console.log(`Title of Activity: ${docs.titleofactivity}`);
-//     console.log("--------------")
-// }
+//      console.log(`Timestamp: ${docs.timestamp}`);
+//      console.log(`Date of Submission: ${docs.dateofsubmission}`);
+//      console.log(`Submitted By: ${docs.submittedby}`);
+//      console.log(`Organization's Name: ${docs.organizationsname}`);
+//      console.log(`Title of Activity: ${docs.titleofactivity}`);
+//      console.log("--------------")
+//  }
 
-// async function accessSpreadsheet() {
-//     await promisify(doc.useServiceAccountAuth)(creds);
-//     const info = await promisify(doc.getInfo)();
-//     const sheet = info.worksheets[0];
-//     const rows = await promisify(sheet.getRows)({
-//         offset: 1
-//     })
-//     docsRows = rows;
-//     rows.forEach(row =>{
-//         printDocument(row)
-//     })
-// }
+ async function accessSpreadsheet() {
+     await promisify(doc.useServiceAccountAuth)(creds);
+     const info = await promisify(doc.getInfo)();
+     const sheet = info.worksheets[0];
+     const rows = await promisify(sheet.getRows)({
+       offset: 1
+    })
+     docsRows = rows;
+     rows.forEach(row =>{
+         console.log(row)
+    })
+ }
 
-// accessSpreadsheet();
+accessSpreadsheet();*/
