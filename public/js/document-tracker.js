cbb9f94716b7bdbc986afe57be82a06418c153f1
unknown <ma_patricia_villaroman@dlsu.edu.ph>
2019-11-12 10:22:27 +0800

    fixed column filtering

--- public/js/document-tracker.js
+++ public/js/document-tracker.js
@@ -9,24 +9,21 @@ $(document).ready(() => {
     $('#myTable thead tr').clone(true).appendTo( '#myTable thead' );
     $('#myTable thead tr:eq(1) th').each( function (i) {
         let title = $(this).text();
-        $(this).html( '<input type="text" placeholder="Search '+title+'" />' );
-
-        $( 'input', this ).on( 'keyup change', function () {
-            if ( table.column(i).search() !== this.value ) {
-                table
-                    .column(i)
-                    .search( this.value )
-                    .draw();
-            }
-        } );
-    } );
-
-    /*
-    "bSortCellsTop": true,
-        fixedHeader: true,
         
-    */
+        
+        $(this).html( '<input type="text" data-index =' + i + ' placeholder="Search '+title+'" />' );
+        $(this).addClass('filter');
+        if (title === 'Remarks' || 'Pended') {
+            $(this).addClass('fixedCol');
+        }
 
-    
-})
+
+        // $('.filter, input', this).on('keyup change', function() {
+        //     //clear global search values
+        //     //table.search('');
+        //     console.log('wow');
+        //     table.column($(this).data('index')).search(this.value).draw();
+        // });
+    });
+});
 
