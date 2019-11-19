4f0fb8de5649c3a2833b05f5b9c500a6eb1d1506
ShashaVillaroman <ma_patricia_villaroman@dlsu.edu.ph>
2019-11-19 19:52:44 +0800

    changed ids, connected sheet update functionality

--- public/js/users.js
+++ public/js/users.js
@@ -13,11 +13,37 @@ $(document).ready(() => {
         key = $(this).parent().parent().data('id');
         //console.log("key: " + key);
         userEl = document.querySelectorAll('[data-id="' + key + '"]')[0];
-        //console.log("userel: " + userEl);
+        console.log("userel: " + userEl);
         let emailEl = userEl.querySelector('.td-email');
-        
+        console.log("emailEl: " + emailEl);
+        console.log("content: " + emailEl.textContent);
         document.getElementById("modal-title").textContent = key;
         document.getElementById("currentEmail").value = emailEl.textContent;
+    });
+
+    $("#editSheetBtn").click(function(e){
+        e.preventDefault();
+        console.log("#editSheetBtn");
+        let sheetValue = $("#newSheet").val();
+        console.log("sheetValue: " + sheetValue);
+        if (sheetValue) {
+            $.ajax({
+                url: "editSheet",
+                method: "POST",
+                data:{
+                    org: key,
+                    sheetKey: sheetValue
+                }, 
+                success: function(result){
+                    console.log("here?");
+                    //location.reload(true);
+                    //place db shit here
+                }
+            });
+        } else {
+            // insert warning
+            console.log("empty?")
+        }
     });
     
 })
