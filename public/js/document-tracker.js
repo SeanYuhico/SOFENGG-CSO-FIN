7d867fccc29bd04c496b4438b75dda8cd9f9f231
ShashaVillaroman <ma_patricia_villaroman@dlsu.edu.ph>
2019-12-10 19:16:04 +0800

    fixed search for document tracker

--- public/js/document-tracker.js
+++ public/js/document-tracker.js
@@ -1,22 +1,24 @@
 $(document).ready(() => {
+    
     $("#balance-nav-link").removeAttr('class', 'active')
     $("#home-nav-link").removeAttr('class', 'active')
     $("#debts-nav-link").removeAttr('class', 'active')
     $("#document-nav-link").attr('class', 'active')
     $("#help-nav-link").removeAttr('class', 'active')
     $("#users-nav-link").removeAttr('class', 'active')
-    table = $('#myTable');
-    $('#myTable thead tr').clone(true).appendTo( '#myTable thead' );
-    $('#myTable thead tr:eq(1) th').each( function (i) {
-        let title = $(this).text();
+    // table = $('#myTable');
+    // $('#myTable thead tr').clone(true).appendTo( '#myTable thead' );
+    // $('#myTable thead tr:eq(1) th').each( function (i) {
+    //     let title = $(this).text();
         
         
-        $(this).html( '<input type="text" data-index =' + i + ' autocomplete="off" placeholder="Search '+title+'" />' );
-        $(this).addClass('filter');
-        if (title === 'Remarks' || 'Pended') {
-            $(this).addClass('fixedCol');
-        }
+    //     $(this).html( '<input type="text" data-index =' + i + ' autocomplete="off" placeholder="Search '+title+'" />' );
+    //     $(this).addClass('filter');
+    //     if (title === 'Remarks' || 'Pended') {
+    //         $(this).addClass('fixedCol');
+    //     }
 
-    });
+    // });
+    
 });
 
