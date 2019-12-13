f83da5434a2b055a69ceeb0b97c4fcaa52cadde2
ShashaVillaroman <ma_patricia_villaroman@dlsu.edu.ph>
2019-12-13 09:58:49 +0800

    added flatpickr

--- public/js/document-tracker.js
+++ public/js/document-tracker.js
@@ -1,12 +1,18 @@
-const flatpickr = require("flatpickr");
-$(document).ready(() => {
-    
+
+
+$(document).ready(() => {    
     $("#balance-nav-link").removeAttr('class', 'active')
     $("#home-nav-link").removeAttr('class', 'active')
     $("#debts-nav-link").removeAttr('class', 'active')
     $("#document-nav-link").attr('class', 'active')
     $("#help-nav-link").removeAttr('class', 'active')
     $("#users-nav-link").removeAttr('class', 'active')
+
+    flatpickr("#col0_filter", {
+        dateFormat: "m/d/Y",
+    });    
+
+    
     // table = $('#myTable');
     // $('#myTable thead tr').clone(true).appendTo( '#myTable thead' );
     // $('#myTable thead tr:eq(1) th').each( function (i) {
